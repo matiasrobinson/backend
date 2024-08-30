c5bebb048b76c7483633bcf9698df25d873f5fc1
matiasbastias <matiasbastias@gmail.com>
2024-08-30 14:12:11 -0400

    edit

--- Ej1.py
+++ Ej1.py
@@ -11,4 +11,10 @@ print("Nombre: ", nombre, "Tipo: ", type(nombre))
 print("Edad: ", edad, "Tipo: ", type(edad))
 print("Altura: ", altura, "Tipo: ", type(altura))
 print("Es mayor de edad: ", es_mayor_de_edad, "Tipo: ", type(es_mayor_de_edad))
-      
+
+radio_del_circulo = 5.0
+constante_pi = 3.14159
+
+area_del_circulo = constante_pi * (radio_del_circulo**2)
+
+print(area_del_circulo)
